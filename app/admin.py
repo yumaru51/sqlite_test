92fc609a586f434172355896b7213b8eb57815a1
y-kawauchi <yumaru51@yahoo.co.jp>
2023-02-16 16:32:38 +0900

    sqlite3マイグレーションテスト

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import TargetMaster
-
-# Register your models here.
+from .models import TargetMaster, PageMaster, ActionMaster, StepMaster, StepPageEntryMaster, StepDisplayPage
 
 
 class TargetMasterAdmin(admin.ModelAdmin):
@@ -11,3 +9,48 @@ class TargetMasterAdmin(admin.ModelAdmin):
 
 
 admin.site.register(TargetMaster, TargetMasterAdmin)
+
+
+class PageMasterAdmin(admin.ModelAdmin):
+    list_display = ('page', 'page_name', 'lost_flag')
+    list_filter = ['page', 'page_name', 'lost_flag']
+    search_fields = ['page', 'page_name', 'lost_flag']
+
+
+admin.site.register(PageMaster, PageMasterAdmin)
+
+
+class ActionMasterAdmin(admin.ModelAdmin):
+    list_display = ('action', 'action_name', 'action_class', 'action_authority', 'progress_transition', 'lost_flag')
+    list_filter = ['action', 'action_name', 'action_class', 'action_authority', 'progress_transition', 'lost_flag']
+    search_fields = ['action', 'action_name', 'action_class', 'action_authority', 'progress_transition', 'lost_flag']
+
+
+admin.site.register(ActionMaster, ActionMasterAdmin)
+
+
+class StepMasterAdmin(admin.ModelAdmin):
+    list_display = ('step', 'step_name', 'hidden_flag', 'lost_flag')
+    list_filter = ['step', 'step_name', 'hidden_flag', 'lost_flag']
+    search_fields = ['step', 'step_name', 'hidden_flag', 'lost_flag']
+
+
+admin.site.register(StepMaster, StepMasterAdmin)
+
+
+class StepPageEntryMasterAdmin(admin.ModelAdmin):
+    list_display = ('step', 'page', 'target', 'lost_flag')
+    list_filter = ['step', 'page', 'target', 'lost_flag']
+    search_fields = ['step', 'page', 'target', 'lost_flag']
+
+
+admin.site.register(StepPageEntryMaster, StepPageEntryMasterAdmin)
+
+
+class StepDisplayPageAdmin(admin.ModelAdmin):
+    list_display = ('step', 'page_no', 'page', 'default_page', 'lost_flag')
+    list_filter = ['step', 'page_no', 'page', 'default_page', 'lost_flag']
+    search_fields = ['step', 'page_no', 'page', 'default_page', 'lost_flag']
+
+
+admin.site.register(StepDisplayPage, StepDisplayPageAdmin)
--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('app.urls'))
+    path('app/', include('app.urls')),
+    # path('isk_tools/report_output/', include('report_output.urls')),
+    # path('isk_tools/quality_change_management/', include('quality_change_management.urls')),
 ]
