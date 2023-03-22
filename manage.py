0b794fc2cea9ac3b7f1a6ce88f7c5929321e5961
y-kawauchi <yumaru51@yahoo.co.jp>
2023-03-22 15:16:16 +0900

    settingsファイルを環境毎に分ける。

--- manage.py
+++ manage.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python
 import os
 import sys
+from socket import gethostname
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    host_name = gethostname()
+    if host_name == 'YWEBSERV1':
+        print('本番環境起動')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings_product')
+    elif host_name == 'I7161DD6':
+        print('テスト環境起動')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings_test')
+    elif host_name == 'I7781DN7':
+        print('開発環境1起動')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings_develop1')
+    else:
+        print('開発環境2起動')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings_develop2')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
